b1a74db3939d089b8d8f022eaa3f1cf419d3e295
Mikalai Chaliadzinau <Rvach.Flyver@gmail.com>
2018-10-04 23:18:04 +0300

    Bundler PoC for dependencies handling

--- build.js
+++ build.js
@@ -1,19 +1,43 @@
 const ACORN = require("acorn");
 const FS = require("fs");
 
+//
+const AST_CONSTS = require("./ast.consts.json")
+
 const SCRIPT = './src/index.js';
 const ACORN_OPTIONS = {
     ecmaVersion: 7,
     sourceType: 'module'
 }
 
-function prettyPrint(json) {
+function PrettyPrint(json) {
     console.log( JSON.stringify(json, null, 2) );
 }
 
 FS.readFile(SCRIPT, (err, data) => {
     if (err) throw err;
     const ast = ACORN.parse(data, ACORN_OPTIONS);
-    prettyPrint(ast);
+    // PrettyPrint(ast);
 });
 
+/**
+ * 1) Went through nodes
+ */
+function Walk(ast) {
+    switch(ast.type) {
+        case "Program": ast.body.forEach( (element, idx, arr) => {
+            arr[idx] = Walk(element);
+        }); break;
+        // import
+        case "ImportDeclaration": ast = AST_CONSTS; break;
+        // case "ImportDefaultSpecifier": Walk(ast.local); break;
+    }
+    // console.log('Walk complete!');
+    return ast;
+}
+
+function TestPoC(){
+    const ast = ACORN.parse("import A from './a.js';", ACORN_OPTIONS);
+    PrettyPrint(Walk(ast));
+}
+TestPoC();
