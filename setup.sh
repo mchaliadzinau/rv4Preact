764cb9a1cb99cc5920f919f2a8ec98a576f027f9
Mikalai Chaliadzinau <Rvach.Flyver@gmail.com>
2018-10-25 17:33:26 +0300

    Remove dependency and updated setup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 set -x
-mkdir -p src/lib/
-cp node_modules/preact/dist/preact.mjs src/lib/preact.mjs
+mkdir -p src/libs/
+cp node_modules/preact/dist/preact.mjs src/libs/preact.mjs
 npm i -g http-server
 set +x
