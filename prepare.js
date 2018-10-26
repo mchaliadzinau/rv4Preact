72d89bbae7a3b22aa2b7dc9ad744669f7ee6d8d3
Mikalai Chaliadzinau <Rvach.Flyver@gmail.com>
2018-10-26 13:38:21 +0300

    Updated prepare.js with example bundling functionality of wrapping IIFEs into es6 module exports

--- prepare.js
+++ prepare.js
@@ -1,22 +1,30 @@
 /**
  * This script copies all dependencies into ./src/libs/ directory
  */
-const fs = require('fs');
+// REQUIREMENTS:
+const FS = require('fs');
 const path = require('path');
+const ACORN = require("acorn");
+const ASTRING = require('astring')
 
+const AST_EXPORT_DEFAULT = require('./ast/ast.export-default.json');
+const package   = require('./package.json');
+
+// ARGS:
 const ARGS = process.argv.slice(2);
-
+// GLOBAL DATA:
 const PATH_LIBS = './src/libs/';
 const PATH_MODULES = './node_modules/';
-
-const package   = require('./package.json');
-const depNames  = Object.keys(package.dependencies);
+const ACORN_OPTIONS = {
+    ecmaVersion: 7
+}
 
 ARGS.length && console.log(ARGS); // handle agruments to distinguish DEV & PROD
 
+const depNames  = Object.keys(package.dependencies);
 const libsPath = path.resolve(__dirname, PATH_LIBS);
-if (!fs.existsSync(libsPath)){
-    fs.mkdirSync(libsPath);
+if (!FS.existsSync(libsPath)){
+    FS.mkdirSync(libsPath);
 }
 
 const depCount = depNames.length;
@@ -25,17 +33,36 @@ for(let i = 0; i < depCount; i++) {
     const packagePath = path.resolve(__dirname, PATH_MODULES, name);
     const packageJSON = require( path.resolve(packagePath, 'package.json') );
     
+    const moduleDestPath = path.resolve(libsPath, `${name}.mjs`);
     if(packageJSON.module) {
         const moduleSourcePath = path.resolve(packagePath, packageJSON.module);
-        const moduleDestPath = path.resolve(libsPath, `${name}.mjs`);
-        fs.createReadStream( moduleSourcePath )
-        .pipe(fs.createWriteStream( moduleDestPath ));
+        FS.createReadStream( moduleSourcePath )
+        .pipe(FS.createWriteStream( moduleDestPath ));
         
         console.log(`OK:\t ${name + package.dependencies[name]} is copied to ${moduleDestPath} directory.` );
-    } else {
-        switch (name) {
-            default: console.error('ERROR:\t ES6 module has not been found for ' + name);
+    } else { // handle situations when package have no es6 module version
+        switch (name) { // TO DO Rework to be able to use plugins instead hardcoded code
+            case 'tree': {
+                const scriptPath = path.resolve(packagePath, 'tree.js');
+                try {
+                    const data = FS.readFileSync(scriptPath);
+                    const ast = ACORN.parse(data, ACORN_OPTIONS);
+                    if(ast.body.length && ast.body[0].type === 'ExpressionStatement') {
+                        const moduleAST = Object.assign({},AST_EXPORT_DEFAULT, {
+                            declaration: ast.body[0]
+                        });
+                        const formattedCode = ASTRING.generate(moduleAST);
+
+                        FS.writeFileSync( moduleDestPath, formattedCode );
+                    }
+                } catch(e) {
+                    console.error('ERROR:\t Cannot read ' + name, e);
+                }
+            }; break;
+            default: 
+                console.error('ERROR:\t ES6 module has not been found for ' + name);
         }    
+        console.log(`OK:\t ${name + package.dependencies[name]} is copied to ${moduleDestPath} directory.` );
     }
     
 }
