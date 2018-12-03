7aaef425fe22ede36ec340fc51ae374429105629
Mikalai Chaliadzinau <Rvach.Flyver@gmail.com>
2018-12-03 12:32:42 +0300

    Implemented $destructuringAssignmentConst for future use in ImportSpecifier

--- ast/astUtils.js
+++ ast/astUtils.js
@@ -11,5 +11,43 @@ module.exports = {
             computed
         }
     ),
+    $destructuringAssignmentConst: (identifiers, props) => {
 
+        const leftProps = identifiers.map(name => (                    {
+            "type": "Property",
+            "method": false,
+            "shorthand": true,
+            "computed": false,
+            "key":      Identifier(name),
+            "kind": "init",
+            "value":    Identifier(name),
+        }));
+        const rightValues = props.map(prop => ({
+            "type": "Property",
+            "method": false,
+            "shorthand": false,
+            "computed": false,
+            "key":      Identifier(prop.name),
+            "value":    prop.valueAst,
+            "kind": "init"
+        }));
+
+        return {
+            "type": "VariableDeclaration",
+            "declarations": [
+                {
+                    "type": "VariableDeclarator",
+                    "id": {
+                        "type": "ObjectPattern",
+                        "properties": leftProps
+                    },
+                    "init": {
+                        "type": "ObjectExpression",
+                        "properties": rightValues
+                    }
+                }
+            ],
+            "kind": "const"
+        };
+    }
 }
