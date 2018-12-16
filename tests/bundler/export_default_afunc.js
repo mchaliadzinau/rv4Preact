10a2372f796ed84a2d01100ff376ebe20a8c9929
Mikalai Chaliadzinau <Rvach.Flyver@gmail.com>
2018-12-16 10:11:18 +0300

    Update test scripts

--- tests/bundler/export_default_afunc.js
+++ tests/bundler/export_default_afunc.js
@@ -1,5 +1,6 @@
-import b from './export_default_bconst.js'
+import b from './export_default_bconst.js';
+import {c} from './export_сconst.js';
 const d = 1;
 export default function() {
-    return 'HI ANONIM! ' + b;
+    return 'HI ANONIM! ' + b + ' : ' + c;
 }
--- tests/bundler/export_default_bconst.js
+++ tests/bundler/export_default_bconst.js
@@ -1,6 +1,7 @@
-
+import {c} from './export_сconst.js';
+import './side-effect.js';
 if(true) {
-    console.log('inside b func')
+    console.log('inside b func', c)
 }
 export const bNd = "ExportNamedDeclaration";
 export default b = "B const text";
