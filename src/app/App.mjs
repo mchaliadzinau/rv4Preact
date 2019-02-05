5c24d4af88581c2ffa4db7299827f1efa3a6d71a
Mikalai Chaliadzinau <Rvach.Flyver@gmail.com>
2019-02-05 13:38:42 +0300

    Created store and connection to it

--- src/app/App.mjs
+++ src/app/App.mjs
@@ -1,6 +1,7 @@
 import { Component } from '/libs/preact.mjs';
-import { _, div, ul, li, h1, h2 } from '/utils/pelems.mjs';
+import { _, div, ul, li, h1, h2, h3 } from '/utils/pelems.mjs';
 import Header from './header/Header.mjs'
+import { connect } from '/libs/unistore/integrations/preact.mjs'
 
 /** Instead of JSX, use: h(type, props, ...children) */
 class Main extends Component {
@@ -16,7 +17,7 @@ class Main extends Component {
 	}
 }
 
-export default class App extends Component {
+class App extends Component {
 	componentDidMount() {
 		this.setState({ message:'Hello!' });
 	}
@@ -26,9 +27,12 @@ export default class App extends Component {
 				Header({ message: state.message, onClickHandler : (e) => {alert('click inside header') }}, 
 					h1(null, "HEADER"),
 					h2(null, "LOWERHEADER"),
+					h3(null,props.count)
 				),
 				_(Main)(null)
 			)
 		);
 	}
 }
+
+export default connect('count')(App);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 // import { createElement, div, h1, h2, h3, button, ul, li } from '../node_modules/preact-hyperscript';
 import { Component, createElement, render } from '/libs/preact.mjs';
 import App from './app/App.mjs';
+import createStore from '/libs/unistore/unistore.mjs'
+import { Provider } from '/libs/unistore/integrations/preact.mjs'
+
+let store = createStore({ count: 1110 })
 
 const h = createElement;
 
-render(h(App), document.body);
+render( h(Provider, {store}, h(App) ), document.body);
